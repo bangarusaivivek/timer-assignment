8835c9041d59c1a20883e56188c93d8726bb8494
bangarusaivivek <bangarusaivivek1996@gmail.com>
2021-05-21 19:29:50 +0530

    correction in theme

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,14 +1,12 @@
 import React, {useState, useEffect} from 'react';
 
 function Timer() {
-    const [background, setBackground] = useState('light');
 	let newTime = new Date();
 	let date = newTime.getDate();
 	let month = newTime.getMonth();
 	let year = newTime.getFullYear();
-	let timer;
-    // const firstTimer = `${date}/${month}/${year} ${newTime.getHours()} : ${newTime.getMinutes()} : ${newTime.getSeconds()}`;
-    const firstTimer = new Date().toString();
+	let timer = null;
+    const firstTimer = `${date}/${month}/${year} ${newTime.getHours()} : ${newTime.getMinutes()} : ${newTime.getSeconds()}`;
 	
 	const displayTimer = () => {
 		let time = new Date();
@@ -17,29 +15,34 @@ function Timer() {
 		let seconds = time.getSeconds();
 		let displayTime = `${hour} : ${minutes} : ${seconds}`
 		document.getElementById('time-block').innerHTML = `${date}/${month}/${year} ${displayTime}`;
-		// console.log(str)
+        resumeTimer();
 	}
-	function timerFunc() {
-		displayTimer();
-		timer = setTimeout(() => {
-			timerFunc();
+	function resumeTimer() {
+        timer = setTimeout(() => {
+			displayTimer();
 		},1000)
 	}
-	const resumeTimer = () => {
-		timerFunc();
-	}
-	const stopTimer = () => {
-		clearTimeout(timer);
+	function stopTimer() {
+        clearTimeout(timer);
 		return;
 	}
+    function handleDropdown(e) {
+        if (e.target.value === 'white') {
+            document.getElementById("main-container").classList.replace('black', 'white');
+        }
+        else {
+            document.getElementById("main-container").classList.replace('white', 'black');
+        }
+    }
+
 	useEffect(() => {
 		resumeTimer();
 	},[])
 
 	return (
-		<div className={`main-container ${background}`}>
+		<div id="main-container" className={`main-container white`}>
 			<div className="dropdown">
-				<select onChange={(e) => setBackground(e.target.value)}>
+				<select onChange={handleDropdown}>
 					<option value="white">Light</option>
 					<option value="black">Dark</option>
 				</select>
