ff050c7a140240a39843cf5e663609aed6335054
bangarusaivivek <bangarusaivivek1996@gmail.com>
2021-05-22 17:09:42 +0530

    added dynamic theme change and used useState hooks to store state

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,55 +1,62 @@
 import React, {useState, useEffect} from 'react';
+import TimerText from './timerText';
 
 function Timer() {
+    const [timerStr, setTimerStr] = useState('');
+    const [cssClass, setCssClass] = useState('white');
+	let [timer, setTimer] = useState(null);
+
 	let newTime = new Date();
 	let date = newTime.getDate();
 	let month = newTime.getMonth();
 	let year = newTime.getFullYear();
-	let timer = null;
-    const firstTimer = `${date}/${month}/${year} ${newTime.getHours()} : ${newTime.getMinutes()} : ${newTime.getSeconds()}`;
+
+    // calls while component mounted.
+    useEffect(() => {
+		resumeTimer();
+	},[])
+
+    useEffect(() => {
+        if (cssClass === 'white') {
+            setCssClass('black')
+        }
+        else {
+            setCssClass('white')
+        }
+    }, [timerStr])
 	
+    // setting time value
 	const displayTimer = () => {
 		let time = new Date();
 		let hour = time.getHours();
 		let minutes = time.getMinutes();
 		let seconds = time.getSeconds();
 		let displayTime = `${hour} : ${minutes} : ${seconds}`
-		document.getElementById('time-block').innerHTML = `${date}/${month}/${year} ${displayTime}`;
-        resumeTimer();
+		setTimerStr(`${date}/${month}/${year} ${displayTime}`);
 	}
+
+    // to resume the time
 	function resumeTimer() {
-        timer = setTimeout(() => {
+        setTimer(setInterval(() => {
 			displayTimer();
-		},1000)
+		},1000))
 	}
+
+    // to stop the time
 	function stopTimer() {
-        clearTimeout(timer);
+        clearInterval(timer);
 		return;
 	}
-    function handleDropdown(e) {
-        if (e.target.value === 'white') {
-            document.getElementById("main-container").classList.replace('black', 'white');
-        }
-        else {
-            document.getElementById("main-container").classList.replace('white', 'black');
-        }
-    }
-
-	useEffect(() => {
-		resumeTimer();
-	},[])
 
 	return (
-		<div id="main-container" className={`main-container white`}>
-			<div className="dropdown">
-				<select onChange={handleDropdown}>
+		<div id="main-container" className={`main-container ${cssClass}`}>
+			{/* <div className="dropdown">
+				<select onChange={(e) => setCssClass(e.target.value)}>
 					<option value="white">Light</option>
 					<option value="black">Dark</option>
 				</select>
-			</div>
-			<div id="time-block" className="time-block">
-				{firstTimer}
-			</div>
+			</div> */}
+			<TimerText props={timerStr}/>
 			<button onClick={stopTimer}>stop</button>
 			<button onClick={resumeTimer}>resume</button>
 		</div>
---
+++ src/components/timerText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TimerText({props}) {
+    return (
+        <div id="time-block" className="time-block">
+            {props}
+        </div>
+    )
+}
+
+export default TimerText;
